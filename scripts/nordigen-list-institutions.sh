67eee8d6583c86cc5085895e9cfbbb0e52c2543a
Pedro Calado <pedrocalado@metric4.io>
2023-03-10 19:12:39 +0000

    feature: Add scripts to help linking nordigen account.

---
+++ scripts/nordigen-list-institutions.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+usage() {
+    echo -e -n "Usage:\n$0 <country-code>\n"
+    echo ""
+    echo "This script list banks available in Nordigen given a coutry ISO Code."
+    echo "Only id and name fields are printed."
+    echo "Refer to https://nordigen.com/en/account_information_documenation/
+    integration/quickstart_guide/ for more details."
+}
+
+if [ $# -lt 1 ]
+then
+    usage
+    exit 1
+fi
+
+source ./scripts/common.sh
+
+export $(xargs < .dev.vars)
+
+TOKEN=$(get_nordigen_token $NORDIGEN_SECRET_ID $NORDIGEN_SECRET_KEY)
+
+curl -X GET "https://ob.nordigen.com/api/v2/institutions/?country=$1" \
+  -H  "accept: application/json" \
+  -H  "Authorization: Bearer ${TOKEN}" | \
+  jq '.[] | {id:.id,name:.name}'
